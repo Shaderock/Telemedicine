b12d267bebef89e4ddcce6a2ef4a6f463ef86e45
Shaderock <hdv98plus@gmail.com>
2019-11-10 15:25:05 +0200

    Initial commit

---
+++ app/src/main/java/com/example/telemedicine/WelcomeScreen.java
@@ -0,0 +1,16 @@
+package com.example.telemedicine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+}
